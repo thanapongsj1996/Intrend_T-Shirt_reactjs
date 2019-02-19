fe096587de8d26138e4e8d2b35a3127306fed5ac
Thanapong <thanapongsj1996@gmail.com>
2019-02-20 04:46:25 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import './App.css'
 import React, { Component } from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Monitor from './components/monitor/Monitor'
-import axios from 'axios'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './containers/Home'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      products: ''
-    }
-  }
-
-  componentDidMount() {
-
-    // this.setState({
-    //   products: [
-    //     {productId: 1, productName: 'เสื้อตามกระแส 1', unitPrice: '250', thumbnail:'/images/products/1.jpg'},
-    //     {productId: 2, productName: 'เสื้อตามกระแส 2', unitPrice: '350', thumbnail:'/images/products/2.jpg'},
-    //     {productId: 3, productName: 'เสื้อตามกระแส 3', unitPrice: '450', thumbnail:'/images/products/3.jpg'}
-    //   ]
-    // })
-
-    // fetch('http://localhost:3001/products', { method: 'GET' })
-    //   .then(res => res.json())
-    //   .then(res => { this.setState({ products: res }) })
-
-    axios.get('http://localhost:3001/products').then(res => {
-      console.log(res.data)
-      this.setState({ products: res.data })
-    })
+  renderRouter(){
+    return(
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+    )
   }
 
   render() {
     return (
-      <div>
-        <Header />
-        <Monitor products={this.state.products} />
-        <Footer company='Intrend T-Shirt' email='thanapongsj1996@gmail.com' />
-      </div>
+      <BrowserRouter>{this.renderRouter()}</BrowserRouter>
     );
   }
 }
