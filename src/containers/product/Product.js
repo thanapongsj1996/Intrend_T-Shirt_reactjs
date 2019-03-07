9562a436bf1410727d12abc810ab8dacdd3544a2
Thanapong <thanapongsj1996@gmail.com>
2019-03-07 22:39:48 +0700

    redux complete

--- src/containers/product/Product.js
+++ src/containers/product/Product.js
@@ -3,42 +3,27 @@ import { withRouter } from 'react-router-dom'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import ProductList from '../../components/product/ProductList'
-import axios from 'axios'
+import { connect } from 'react-redux'
+import { productsFetch, productDelete } from '../../actions'
 
 class Product extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            products: null
-        }
         this.editProduct = this.editProduct.bind(this)
         this.deleteOrder = this.deleteOrder.bind(this)
     }
 
     componentDidMount() {
-        axios.get('http://localhost:3001/shirts')
-            .then(res => {
-                this.setState({
-                    products: res.data
-                })
-            })
+        this.props.productsFetch()
     }
 
-    editProduct(product){
+    editProduct(product) {
         this.props.history.push('products/edit/' + product.id)
     }
 
     deleteOrder(product) {
-        axios.delete("http://localhost:3001/shirts/" + product.id)
-            .then(res => {
-                axios.get('http://localhost:3001/shirts')
-                    .then(res => {
-                        this.setState({
-                            products: res.data
-                        })
-                    })
-            })
+        this.props.productDelete(product.id)
     }
 
     render() {
@@ -51,13 +36,13 @@ class Product extends Component {
                             <h1>สินค้า</h1>
                         </div>
                         <div className='col-6'>
-                            <button className='btn btn-success title float-right' onClick={()=> this.props.history.push('products/add')}>เพิ่ม</button>
+                            <button className='btn btn-success title float-right' onClick={() => this.props.history.push('products/add')}>เพิ่ม</button>
                         </div>
                     </div>
-                    <ProductList 
-                        products={this.state.products}
+                    <ProductList
+                        products={this.props.products}
                         onEditProduct={this.editProduct}
-                        onDeleteProduct={this.deleteOrder} 
+                        onDeleteProduct={this.deleteOrder}
                     />
                 </div>
                 <Footer company='Intrend T-Shirt' email='thanapongsj1996@gmail.com' />
@@ -67,4 +52,8 @@ class Product extends Component {
 
 }
 
-export default withRouter(Product)
+function mapStateToProps({ products }) {
+    return { products }
+}
+
+export default withRouter(connect(mapStateToProps, { productsFetch, productDelete })(Product))
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
+import reducers from './reducers/index';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
